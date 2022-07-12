04d072925ee2dd796fddd7ae6605e7f2c53e65fe
lutfiqaraman <lutfiqaraman@gmail.com>
2022-07-12 21:43:17 +0300

    update a form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from "@angular/core";
 import {UserSettingsFormComponent} from "./user-settings-form/user-settings-form.component";
 import {SimpleformComponent} from "./simpleform/simpleform.component";
 import {ReactiveformComponent} from "./reactiveform/reactiveform.component";
+import {UpdateformComponent} from "./updateform/updateform.component";
 
 
 const routes: Routes = [
   { path: '', component: UserSettingsFormComponent },
   { path: 'simpleform', component: SimpleformComponent },
-  { path: 'reactiveform', component: ReactiveformComponent }
+  { path: 'reactiveform', component: ReactiveformComponent },
+  { path: 'updateform', component: UpdateformComponent }
 ];
 
 @NgModule({
